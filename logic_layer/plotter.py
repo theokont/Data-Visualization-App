c3e897f704b1a758a9afc05a2fa5a2d73244a628
Theofilos Kontaxis <theo.kontaxis@gmail.com>
2019-05-26 21:28:39 +0300

    Update plotter.py

--- logic_layer/plotter.py
+++ logic_layer/plotter.py
@@ -8,21 +8,6 @@ class Plotter:
     def __init__(self):
         self.d = timedelta(days=1)
 
-    def daily_plot(self, mode, x, y):
-        self.graphic_object = go.Scatter(
-            x=x,
-            y=y,
-            mode=mode)
-        self.data = [self.graphic_object]
-
-        self.layout = go.Layout(xaxis=dict(
-            range=[x[0]-self.d,
-                   x[-1]+self.d]
-        ))
-
-        self.fig = go.Figure(data=self.data, layout=self.layout)
-        plotly.offline.plot(self.fig, filename='daily.html', auto_open=True)
-
     def scatter_plot(self, mode, x, y, title, x_axis_title, y_axis_title):
         self.graphic_object = go.Scatter(
             x=x,
@@ -56,11 +41,10 @@ class Plotter:
         self.fig = go.Figure(data=self.data, layout=self.layout)
         plotly.offline.plot(self.fig, filename="%s.html" % title, auto_open=True)
 
-    def yearly_plot(self, mode, x, y, title, x_axis_title, y_axis_title):
-        self.graphic_object = go.Scatter(
+    def bar_plot(self, x, y, title, x_axis_title, y_axis_title):
+        self.graphic_object = go.Bar(
             x=x,
-            y=y,
-            mode=mode)
+            y=y)
         self.data = [self.graphic_object]
 
         self.layout = go.Layout(xaxis=dict(
@@ -71,20 +55,20 @@ class Plotter:
             annotations=[
             dict(
                 x=0.5,
-                y=-0.09,
+                y=-0.065,
                 showarrow=False,
                 text=x_axis_title,
                 xref='paper',
                 yref='paper'
             ),
             dict(
-                x=-0.04,
+                x=-0.03,
                 y=0.5,
                 showarrow=False,
-                text="UNIT - %s" % y_axis_title,
+                text="UNIT - {}".format(y_axis_title),
                 textangle=-90,
                 xref='paper',
                 yref='paper')
         ])
         self.fig = go.Figure(data=self.data, layout=self.layout)
-        plotly.offline.plot(self.fig, filename='yearly.html', auto_open=True)
+        plotly.offline.plot(self.fig, filename="%s.html" % title, auto_open=True)
