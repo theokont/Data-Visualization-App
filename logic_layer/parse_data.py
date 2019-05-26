99b0a34d19d8d42beccb4f25bc482a1cf2d19cc0
Theofilos Kontaxis <theo.kontaxis@gmail.com>
2019-05-26 21:27:17 +0300

    Update parse_data.py

--- logic_layer/parse_data.py
+++ logic_layer/parse_data.py
@@ -1,4 +1,3 @@
-# import re
 import xml.etree.ElementTree as ET
 
 
@@ -13,28 +12,12 @@ class Parser:
 
         for sensor_unit in self.root.iter('UNIT'):
             self.unit = sensor_unit.text
-            # return self.id
-            # print(self.unit)
+
         for value in self.root.iter('VALUE'):
             self.val = value.text
-            # self.int_val = int(self.val)
-            # return self.val
-            # print(self.int_val)
+
         for el in self.root.iter('TIME'):
             self.time_stamp = el.text
-            # self.re_compiled = re.compile(r"(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2})")
-            # self.re_result = self.re_compiled.match(self.time_stamp)
-            # self.date = self.re_result.group(1)
-            # self.time = self.re_result.group(2)
-
-            # return self.time_stamp
-            # print('Date: {}, Time: {}'.format(self.date, self.time))
-
-        # for table_name in self.root.iter('Site'):
-        #     self.name = table_name.attrib['NAME']
-        #     # return self.name
-        #     print(self.name)
-        # if self.unit == "CM":
 
         return self.unit, self.val, self.time_stamp
 
