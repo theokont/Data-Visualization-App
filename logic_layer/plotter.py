b5c93eb6e41479d5c931f4d0f920d8191d3c9068
Theofilos Kontaxis <theo.kontaxis@gmail.com>
2019-06-10 12:44:49 +0300

    Update plotter.py

--- logic_layer/plotter.py
+++ logic_layer/plotter.py
@@ -8,6 +8,47 @@ class Plotter:
     def __init__(self):
         self.d = timedelta(days=1)
 
+    def go_bar(self, mode, x, y, title):
+        self.graphic_bar_object = go.Bar(
+            x=x,
+            y=y,
+            name=title[:-3]
+        )
+
+        return self.graphic_bar_object
+
+    def go_bar_plot(self, title, x_axis_title, y_axis_title, data):
+        self.layout = go.Layout(
+            title=title,
+            autosize=True,
+            annotations=[
+                dict(
+                    x=0.5,
+                    y=-0.15,  # -0.065
+                    showarrow=False,
+                    text=x_axis_title,
+                    xref='paper',
+                    yref='paper'
+                ),
+                dict(
+                    x=-0.1,  # -0.03
+                    y=0.5,
+                    showarrow=False,
+                    text="UNIT - {}".format(y_axis_title),
+                    textangle=-90,
+                    xref='paper',
+                    yref='paper')
+            ],
+
+
+            barmode='group',
+            bargap=0.15,
+            bargroupgap=0.1
+        )
+
+        self.fig = go.Figure(data=data, layout=self.layout)
+        plotly.offline.plot(self.fig, filename="%s.html" % title, auto_open=True)
+
     def go_scatter(self, mode, x, y, title):
         self.graphic_object = go.Scatter(
             x=x,
